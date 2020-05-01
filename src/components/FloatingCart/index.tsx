c3703f9e09221d023d6913236a0d96abe724d583
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-05-01 16:11:50 -0300

    Add calculate total in hook

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -25,14 +25,21 @@ const FloatingCart: React.FC = () => {
 
   const cartTotal = useMemo(() => {
     // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const value = products.reduce((acc, product) => {
+      const total = product.price * (product.quantity || 1);
+      return acc + total;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(value);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const quant = products.reduce((acc, product) => {
+      return acc + (product.quantity || 1);
+    }, 0);
 
-    return 0;
+    return quant;
   }, [products]);
 
   return (
